b66478b496ed4c07b3c7adb27249e5267f233ce5
JHickson17 <160874848+JHickson17@users.noreply.github.com>
2024-03-11 22:17:28 +0000

    Update database.py

--- database.py
+++ database.py
@@ -13,16 +13,17 @@ Money     INTEGER)""")
 
 def create_statistics_table():
     cur.execute("""CREATE TABLE Statistics
-(Username       INTEGER,
-RoundsPlayed    TEXT,
-RoundsWon       TEXT,
-RoundsLost      TEXT,
-RoundsDrawn     TEXT,
+(Username       TEXT,
+RoundsPlayed    INTEGER,
+RoundsWon       INTEGER,
+RoundsLost      INTEGER,
+RoundsDrawn     INTEGER,
 FOREIGN KEY (Username) REFERENCES Users (Username))""")
 
 
 def add_user(username, password, money):
     cur.execute("INSERT INTO Users VALUES (?, ?, ?)", (username, password, money))
+    cur.execute("INSERT INTO Statistics VALUES (?, ?, ?, ?, ?)", (username, 0, 0, 0, 0))
     connection.commit()
 
 def delete_record(username):
@@ -35,5 +36,28 @@ def find_user(username):
     return userDetails
 
 def update_money(username, money):
-    cur.execute("UPDATE Users SET Money = ? WHERE Username = ?", (money, username))
+    cur.execute("""UPDATE Users 
+                   SET   Money = ? 
+                   WHERE Username = ?""", (money, username))
     connection.commit()
+
+def load_leaderboard():
+    cur.execute("""SELECT Username, Money
+                   FROM   Users
+                   ORDER BY Money DESC""")
+    leaderboard = cur.fetchall()
+    return leaderboard
+
+def update_statistics(username, roundsPlayed, roundsWon, roundsLost, roundsDrawn):
+    cur.execute("""UPDATE Statistics 
+                   SET (RoundsPlayed, RoundsWon, RoundsLost, RoundsDrawn) = (?, ?, ?, ?) 
+                   WHERE Username = ?""", (roundsPlayed, roundsWon, roundsLost, roundsDrawn, username))
+    connection.commit()
+
+def load_statistics(username):
+    cur.execute("""SELECT roundsPlayed, roundsWon, roundsLost, roundsDrawn
+                   FROM   Statistics
+                   WHERE Username = ?""", (username,))
+    statistics = cur.fetchall()
+    return statistics
+
