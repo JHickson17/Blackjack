0534e84635bfc3c801690e33b0c509d5920c1f50
JHickson17 <160874848+JHickson17@users.noreply.github.com>
2024-03-13 22:33:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ FPS = 60
 
 WIDTH = 1200
 HEIGHT = 700   #Height and width of the screen
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 GREEN = (39, 130, 51)       #RGB values for the colours
 BLACK =(20, 20, 20)
@@ -21,13 +21,13 @@ YELLOW = (255, 255, 0)
 
 MENU_BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("images", "menu background.XCF")), (WIDTH, HEIGHT))
 
-def blackjack_display(player, dealer, betMade, standPressed):
-    screen.fill(GREEN)
+def blackjack_display(player, dealer, betMade, standPressed, doublePressed):
+    SCREEN.fill(GREEN)
 
     rect1 = pygame.Rect(900, 0, 400, 700)
     rect2 = pygame.Rect(925, 520, 250, 50)
-    pygame.draw.rect(screen, BLACK, rect1)
-    pygame.draw.rect(screen, WHITE, rect2)
+    pygame.draw.rect(SCREEN, BLACK, rect1)
+    pygame.draw.rect(SCREEN, WHITE, rect2)
 
     if betMade:
         display_player_cards(player, dealer)  #Once the player has placed their bet it will display their cards
@@ -35,40 +35,60 @@ def blackjack_display(player, dealer, betMade, standPressed):
     if standPressed:
         display_dealer_cards(dealer)  #Once the player has pressed stand it will display the dealer's cards
 
-    display_buttons(betMade)    #Displays the buttons
+    display_buttons(betMade, player, doublePressed)    #Displays the buttons
     display_account()   #Displays the account that the player has signed in to
 
-    player.display_money(screen)    #Displays how much money the player has
-    player.display_bet(screen, betMade) #Displays the player's bet
+    player.display_money(SCREEN)    #Displays how much money the player has
+    player.display_bet(SCREEN, betMade) #Displays the player's bet
 
-    player.display_result(screen)   #Once the user has won, lost or drawn it will display the result and how much money they have won or lost
+    player.display_result(SCREEN)   #Once the user has won, lost or drawn it will display the result and how much money they have won or lost
     
     pygame.display.update()
 
     if player.won or player.draw or player.lost:
         time.sleep(3)       #Pauses the program for 3 seconds at the end of the round
 
-def display_buttons(betMade):   #Displays all of the buttons
-    decreaseBet.display_button(screen)  #Displays the button that decreases the bet
-    increaseBet.display_button(screen)  #Displays the button that increases the bet
-    placeBet.display_button(screen) #Displays the button that places the bet
-    returnToMenu.display_button(screen)
+def check_for_split(player):
+    card1 = player.playerCards[0][:-1]
+    card2 = player.playerCards[1][:-1]
+    if card1 == card2:
+        return True
+    else:
+        return False
+
+def check_for_double(player):
+    card1 = player.playerCards[0][:-1]
+    card2 = player.playerCards[1][:-1]
+    if player.playerTotal >= 9 and player.playerTotal <= 11:
+        if card1 != "1" and card2 != "1":
+            return True
+    return False
+
+def display_buttons(betMade, player, doublePressed):   #Displays all of the buttons
+    decreaseBet.display_button(SCREEN)  #Displays the button that decreases the bet
+    increaseBet.display_button(SCREEN)  #Displays the button that increases the bet
+    placeBet.display_button(SCREEN) #Displays the button that places the bet
+    returnToMenu.display_button(SCREEN)
 
     if betMade:
-        hit.display_button(screen)  #Only displays the hit and stand button once the bet has been placed
-        stand.display_button(screen)
+        hit.display_button(SCREEN)  #Only displays the hit and stand button once the bet has been placed
+        stand.display_button(SCREEN)
 
+        if check_for_split(player):
+            split.display_button(SCREEN)
+
+        if check_for_double(player) and not doublePressed:
+            double.display_button(SCREEN)
         
-
 def display_player_cards(player, dealer):    #Displays the player's cards
     start_x_position = (WIDTH / 2) - dealer.cardWidth
     for i in range(player.numOfPlayerCards): 
-        screen.blit(player.playerCardImages[i], ((start_x_position + (dealer.cardWidth*i) + (5*i)-150), 530))
+        SCREEN.blit(player.playerCardImages[i], ((start_x_position + (dealer.cardWidth*i) + (5*i)-150), 530))
 
 def display_dealer_cards(dealer): #Displays the dealer's cards
     start_x_position = (WIDTH / 2) - dealer.cardWidth
     for i in range(dealer.numOfDealerCards): 
-        screen.blit(dealer.dealerCardImages[i], ((start_x_position + (dealer.cardWidth*i) + (5*i)-150), 20)) 
+        SCREEN.blit(dealer.dealerCardImages[i], ((start_x_position + (dealer.cardWidth*i) + (5*i)-150), 20)) 
 
 def mouse_rect():   #Creates a rect on the mouse and returns it
     mousePos = pygame.mouse.get_pos()
@@ -132,6 +152,7 @@ def blackjack():
 
     betMade = False
     standPressed = False
+    doublePressed = False
     dealerTurnDone = False
     roundEnd = False
     startRound = True
@@ -152,6 +173,9 @@ def blackjack():
             dealer.load_dealer_images()   #Loads the dealers card images into an array
             startRound = False
 
+        canDouble = check_for_double(player)
+        canSplit = check_for_split(player)
+
         if betMade == False:    #Checks if the player hasn't made a bet
             lastPressed = change_bet(player, mouse, count, lastPressed) #Lets the player change their bet
 
@@ -169,6 +193,15 @@ def blackjack():
         if stand.button_pressed(mouse) and count > (lastPressed+60): #Checks if the player presses stand
             standPressed = True
 
+        if betMade and double.button_pressed(mouse) and canDouble:
+            player.money -= player.bet
+            player.bet *= 2
+            standPressed = True
+            doublePressed = True
+            player.deal_player_card()
+            player.convert_player_card(player.lastPlayerCardDealt)
+            player.load_player_images()
+
         if standPressed and not player.won: #Checks if the player has pressed stand and that they haven't already won
             dealerTurnDone = dealer.dealer_turn() #Gives the dealer a card if their total is 16 or less
             dealer.load_dealer_images()   #Loads the image for the card and adds it to the array with the other images
@@ -181,7 +214,7 @@ def blackjack():
         if player.won:  #Checks if the player has won
             player.win()    #Pays the player double their bet
         
-        blackjack_display(player, dealer, betMade, standPressed)
+        blackjack_display(player, dealer, betMade, standPressed, doublePressed)
 
         if player.lost or player.won or player.draw:    #Checks if the round has ended
             roundEnd = True
@@ -212,20 +245,20 @@ def display_account():
     if username.input != "":    #Checks if the player is signed in
         font = pygame.font.SysFont("dejavuserif", 30)
         displayusername = font.render("Account: " + username.input, 1, RED)
-        screen.blit(displayusername, (50, 650))     #Displays the player's account
+        SCREEN.blit(displayusername, (50, 650))     #Displays the player's account
 
 def display_menu():
-    screen.blit(MENU_BACKGROUND, (0, 0))
+    SCREEN.blit(MENU_BACKGROUND, (0, 0))
 
     display_account()
-    play.display_button(screen)     #Displays all of the buttons in the menu
-    signIn.display_button(screen)
-    createAcc.display_button(screen)
-    leaderboardButton.display_button(screen)
+    play.display_button(SCREEN)     #Displays all of the buttons in the menu
+    signIn.display_button(SCREEN)
+    createAcc.display_button(SCREEN)
+    leaderboardButton.display_button(SCREEN)
     if username.input != "":
-        statisticsButton.display_button(screen)
-        signOut.display_button(screen)
-    quit.display_button(screen)
+        statisticsButton.display_button(SCREEN)
+        signOut.display_button(SCREEN)
+    quit.display_button(SCREEN)
 
     pygame.display.update() #Updates the display
 
@@ -266,13 +299,13 @@ def menu():
         display_menu()  #Displays the menu
 
 def sign_in_display():
-    screen.blit(MENU_BACKGROUND, (0, 0))    #Background image
+    SCREEN.blit(MENU_BACKGROUND, (0, 0))    #Background image
 
-    username.display_box(screen)    #Displays the input boxes for the username and password
-    password.display_box(screen)
+    username.display_box(SCREEN)    #Displays the input boxes for the username and password
+    password.display_box(SCREEN)
 
-    back.display_button(screen)     #Displays the buttons to confirm the sign in or go back to the menu
-    confirm.display_button(screen)
+    back.display_button(SCREEN)     #Displays the buttons to confirm the sign in or go back to the menu
+    confirm.display_button(SCREEN)
 
     pygame.display.update()     #Updated the display
 
@@ -291,14 +324,14 @@ def check_details(mouse):
 def signed_in():
     font = pygame.font.SysFont("dejavuserif", 30)
     message = font.render("Signed in!", 1, RED)
-    screen.blit(message, (530, 450))
+    SCREEN.blit(message, (530, 450))
     pygame.display.update()
     time.sleep(2)
 
 def incorrect_details():
     font = pygame.font.SysFont("dejavuserif", 30)
     errorMessage = font.render("Incorrect details. Try again.", 1, RED)
-    screen.blit(errorMessage, (440, 450))
+    SCREEN.blit(errorMessage, (440, 450))
     pygame.display.update()
     time.sleep(2)
 
@@ -335,14 +368,14 @@ def sign_in():
         sign_in_display()   #Displays the sign in menu
 
 def create_account_display(mouse, validPassword):
-    screen.blit(MENU_BACKGROUND, (0, 0))
+    SCREEN.blit(MENU_BACKGROUND, (0, 0))
 
-    username.display_box(screen)
-    password.display_box(screen)
-    confirmPassword.display_box(screen)
+    username.display_box(SCREEN)
+    password.display_box(SCREEN)
+    confirmPassword.display_box(SCREEN)
 
-    back.display_button(screen)
-    confirm.display_button(screen)
+    back.display_button(SCREEN)
+    confirm.display_button(SCREEN)
 
     if validPassword == "invalid" and confirm.button_pressed(mouse):
             password_error()
@@ -356,28 +389,28 @@ def create_account_display(mouse, validPassword):
 def password_error():
     font = pygame.font.SysFont("dejavuserif", 30)
     errorMessage = font.render("Password must be between 8 and 20 character and contain at least 1 number", 1, RED)
-    screen.blit(errorMessage, (150, 515))
+    SCREEN.blit(errorMessage, (150, 515))
     pygame.display.update()
     time.sleep(2)
 
 def existing_username():
     font = pygame.font.SysFont("dejavuserif", 30)
     errorMessage = font.render("Username taken. Try again", 1, RED)
-    screen.blit(errorMessage, (425, 515))
+    SCREEN.blit(errorMessage, (425, 515))
     pygame.display.update()
     time.sleep(2)
 
 def passwords_not_matching():
     font = pygame.font.SysFont("dejavuserif", 30)
     errorMessage = font.render("Passwords do not match.", 1, RED)
-    screen.blit(errorMessage, (425, 515))
+    SCREEN.blit(errorMessage, (425, 515))
     pygame.display.update()
     time.sleep(2)
 
 def account_created():
     font = pygame.font.SysFont("dejavuserif", 30)
     message = font.render("Account Created!", 1, RED)
-    screen.blit(message, (490, 515))
+    SCREEN.blit(message, (490, 515))
     pygame.display.update()
     time.sleep(2)
 
@@ -446,7 +479,7 @@ def leaderboard():
                 return "quit"
             
         mouse = mouse_rect()
-        screen.blit(MENU_BACKGROUND, (0, 0))
+        SCREEN.blit(MENU_BACKGROUND, (0, 0))
 
         if len(players) < 10:
             n = len(players)
@@ -461,8 +494,8 @@ def leaderboard():
                 colour = RED
             font = pygame.font.SysFont("dejavuserif", 45)
             text = font.render(leaderboardPlace+". "+str(player[0])+"    £"+str(player[1]), 1, colour)
-            screen.blit(text, (450, 135+(50*i)))
-        returnToMenu.display_button(screen)
+            SCREEN.blit(text, (450, 135+(50*i)))
+        returnToMenu.display_button(SCREEN)
         if returnToMenu.button_pressed(mouse):
             break
         pygame.display.update()
@@ -479,17 +512,17 @@ def statistics():
                 return "quit"
             
         mouse = mouse_rect()
-        screen.blit(MENU_BACKGROUND, (0, 0))
+        SCREEN.blit(MENU_BACKGROUND, (0, 0))
         rect = pygame.Rect(120, 200, 1000, 100)
-        pygame.draw.rect(screen, GREY, rect, 0, 10)
+        pygame.draw.rect(SCREEN, GREY, rect, 0, 10)
 
         font = pygame.font.SysFont("dejavuserif", 37)
         text = font.render("Rounds Played     Rounds Won     Rounds Lost     Rounds Drawn", 1, BLACK)
-        screen.blit(text, (140, 200))
+        SCREEN.blit(text, (140, 200))
         for i in range(4):
             statistic = font.render(str(stats[i]), 1, RED)
-            screen.blit(statistic, (230+(i*250), 250))
-        returnToMenu.display_button(screen)
+            SCREEN.blit(statistic, (230+(i*250), 250))
+        returnToMenu.display_button(SCREEN)
         if returnToMenu.button_pressed(mouse):
             break
         pygame.display.update()
